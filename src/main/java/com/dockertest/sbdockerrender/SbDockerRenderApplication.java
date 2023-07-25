e53db6104b9774ecf50a9ec517e9496bdece013c
Julian Rojas <julianrojasgeo@gmail.com>
2023-07-25 08:52:09 -0500

    Initial commit

---
+++ src/main/java/com/dockertest/sbdockerrender/SbDockerRenderApplication.java
@@ -0,0 +1,13 @@
+package com.dockertest.sbdockerrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDockerRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDockerRenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockertest/sbdockerrender/SbDockerRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockertest.sbdockerrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDockerRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
